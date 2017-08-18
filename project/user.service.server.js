29c7c3bf75e74228225b470e4c66b85954fdc286
rakeshr99 <rakeshr99@ccs.neu.edu>
2017-08-18 01:42:05 -0400

    saving env variable

--- project/user.service.server.js
+++ project/user.service.server.js
@@ -37,9 +37,9 @@ app.get('/google/callback',
     }));
 
 var googleConfig = {
-    clientID     : GOOGLE_CLIENT_ID,//process.env.GOOGLE_CLIENT_ID,
-    clientSecret : GOOGLE_CLIENT_SECRET,//process.env.GOOGLE_CLIENT_SECRET, //process.env.GOOGLE_CALLBACK_URL
-    callbackURL  : GOOGLE_CALLBACK_URL
+    clientID     : process.env.GOOGLE_CLIENT_ID,//process.env.GOOGLE_CLIENT_ID,
+    clientSecret : process.env.GOOGLE_CLIENT_SECRET,//process.env.GOOGLE_CLIENT_SECRET, //process.env.GOOGLE_CALLBACK_URL
+    callbackURL  : process.env.GOOGLE_CALLBACK_URL
 };
 
 passport.use(new GoogleStrategy(googleConfig, googleStrategy));
